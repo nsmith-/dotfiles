908672cda6f48b5999e0e111ff0532fa611b973d
Nick Smith <nick.smith@cern.ch>
2018-10-30 21:18:45 -0500

    lpc

--- .bashrc.d/lpc.fnal.gov.sh
+++ .bashrc.d/lpc.fnal.gov.sh
@@ -3,10 +3,10 @@ echo $HOSTNAME|grep -q '.fnal.gov' || return
 
 # For scram
 . /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc6_amd64_gcc481
+. /etc/bashrc
+export SCRAM_ARCH=slc6_amd64_gcc630
 
 # For tmux install
 export PATH=~/local/bin:$PATH
 
 # Shortcuts
-export cmsdas=/uscms/home/ncsmith/nobackup/cmsdas/CMSSW_7_3_0_pre1/src
